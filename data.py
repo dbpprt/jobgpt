2eb48ab7076b92558c8c1e4dcb96de0930b6f737
Dennis Bappert <dennisbappert@users.noreply.github.com>
2023-06-13 10:45:09 +0200

    first commit

---
+++ data.py
@@ -0,0 +1,117 @@
+import random
+import re
+from accelerate import Accelerator
+
+# pyright: reportPrivateImportUsage=false
+from datasets import load_dataset
+import torch
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+
+
+def get_dataloaders(accelerator: Accelerator, model_name: str, batch_size: int, seq_len: int):
+    # some boilerplate required to work with different model architectures
+    # TODO: really required?
+    if any(k in model_name for k in ("gpt", "opt", "bloom")):
+        padding_side = "left"
+    else:
+        padding_side = "right"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side=padding_side)
+    if getattr(tokenizer, "pad_token_id") is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+    # attempt to fix "Cannot handle batch sizes > 1 if no padding token is defined." with gpt2
+    # if getattr(tokenizer, "pad_token") is None:
+    #     tokenizer.pad_token = tokenizer.eos_token
+
+    def collate_fn(examples):
+        return tokenizer.pad(examples, padding="longest", return_tensors="pt")
+
+    def __preprocess_function(qualifications, descriptions):
+        batch_size = len(qualifications)
+        inputs = [
+            f"Write a modern and engaging job posting for the following basic qualifications: {x}\r\nResponse: \r\n"
+            for x in qualifications
+        ]
+        targets = [str(x) for x in descriptions]
+        model_inputs = tokenizer(inputs)
+        labels = tokenizer(targets)
+
+        # (heavily) inspired by hf peft example
+        for i in range(batch_size):
+            sample_input_ids = model_inputs["input_ids"][i]
+            label_input_ids = labels["input_ids"][i] + [tokenizer.pad_token_id]
+            model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
+            # all labels set to -100 are ignored (masked), the loss is only computed for the labels set to [0, ..., config.vocab_size - 1]
+            labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
+            model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
+        for i in range(batch_size):
+            sample_input_ids = model_inputs["input_ids"][i]
+            label_input_ids = labels["input_ids"][i]
+            model_inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
+                seq_len - len(sample_input_ids)
+            ) + sample_input_ids
+            model_inputs["attention_mask"][i] = [0] * (
+                seq_len - len(sample_input_ids)
+            ) + model_inputs["attention_mask"][i]
+            labels["input_ids"][i] = [-100] * (seq_len - len(sample_input_ids)) + label_input_ids
+            model_inputs["input_ids"][i] = torch.tensor(model_inputs["input_ids"][i][:seq_len])
+            model_inputs["attention_mask"][i] = torch.tensor(
+                model_inputs["attention_mask"][i][:seq_len]
+            )
+            labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:seq_len])
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
+
+    def preprocess_function(examples: dict):
+        qualifications = [
+            x.replace("· ", "\r\n- ") for x in examples["BASIC QUALIFICATIONS"] if x is not None
+        ]
+        preferred_qualifications = [
+            x.replace("· ", "\r\n- ") for x in examples["PREFERRED QUALIFICATIONS"] if x is not None
+        ]
+
+        qualifications = [f"{x}{y}" for x, y in zip(qualifications, preferred_qualifications)]
+
+        for idx, qualification in enumerate(qualifications):
+            qualification = qualification.split("\r\n- ")
+            # remove empty strings
+            qualification = [x for x in qualification if x]
+            random.shuffle(qualification)
+            qualifications[idx] = "\r\n- " + "\r\n- ".join(qualification)
+
+        # fix some formatting issues in the dataset
+        descriptions = [x.replace("· ", "\r\n- ") for x in examples["DESCRIPTION"] if x is not None]
+        descriptions = [re.sub(r"([.\-,;:!?])(?=\S)", r"\1 ", x) for x in descriptions]
+
+        return __preprocess_function(qualifications, descriptions)
+
+    data_files = {"train": "./data/amazon_jobs_dataset.csv"}
+
+    dataset = load_dataset(
+        "csv",
+        data_files=data_files,
+    )
+
+    with accelerator.main_process_first():
+        dataset = dataset.map(
+            preprocess_function,
+            batched=True,
+            num_proc=1,
+            remove_columns=dataset["train"].column_names,
+            # ideally we should use the cache, but this is a sample script, hence we don't
+            load_from_cache_file=False,
+            desc="Running tokenizer on dataset",
+        )
+
+    accelerator.wait_for_everyone()
+
+    train_dataloader = DataLoader(
+        dataset["train"],
+        shuffle=True,
+        collate_fn=collate_fn,
+        batch_size=batch_size,
+        pin_memory=True,
+    )
+
+    return train_dataloader, tokenizer
---
+++ main.py
@@ -0,0 +1,180 @@
+import argparse
+import os
+import torch
+from accelerate import Accelerator
+from accelerate.utils import find_executable_batch_size
+from peft import LoraConfig, get_peft_model, get_peft_model_state_dict
+from transformers import (
+    AutoModelForCausalLM,
+    get_linear_schedule_with_warmup,
+    set_seed,
+)
+
+from data import get_dataloaders
+
+
+def main(args):
+    accelerator = Accelerator()
+
+    peft_config = LoraConfig(
+        bias="none",
+        task_type="CAUSAL_LM",
+        inference_mode=False,
+        r=args.lora_r,
+        lora_alpha=args.lora_alpha,
+        lora_dropout=args.lora_dropout,
+    )
+
+    # TODO: we currently always set the seed which might not be required in all cases
+    set_seed(args.seed)
+
+    with accelerator.main_process_first():
+        model = AutoModelForCausalLM.from_pretrained(args.model_name, torch_dtype=torch.float16)
+
+    # small fix to get gpt style generation working
+    model.config.pad_token_id = model.config.eos_token_id
+
+    model = get_peft_model(model, peft_config)
+    # prints some nice information about the trainable parameters
+    # and model stastics
+    model.print_trainable_parameters()
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+    model, optimizer = accelerator.prepare(model, optimizer)
+    starting_epoch = 0
+
+    # we wrap the inner training loop into a function
+    # in order to find the maximum batch size that fits into memory
+    @find_executable_batch_size(starting_batch_size=args.batch_size)
+    def training_loop(batch_size: int):
+        nonlocal accelerator  # ensure they can be used in our context
+        accelerator.free_memory()  # free lingering references
+
+        train_dataloader, tokenizer = get_dataloaders(
+            accelerator=accelerator,
+            model_name=args.model_name,
+            batch_size=batch_size,
+            seq_len=args.seq_len,
+        )
+
+        lr_scheduler = get_linear_schedule_with_warmup(
+            optimizer=optimizer,
+            num_warmup_steps=0.06 * (len(train_dataloader) * args.num_epochs),
+            num_training_steps=(len(train_dataloader) * args.num_epochs),
+        )
+
+        train_dataloader, lr_scheduler = accelerator.prepare(train_dataloader, lr_scheduler)
+
+        # investigate if this is necessary
+        # https://github.com/huggingface/accelerate/blob/main/examples/by_feature/automatic_gradient_accumulation.py
+        gradient_accumulation_steps = 1
+        if batch_size < args.batch_size:
+            gradient_accumulation_steps = args.batch_size // batch_size
+            accelerator.print(
+                f"Batch size {args.batch_size} is too large for memory, reducing to {batch_size} and accumulating gradients over {gradient_accumulation_steps} steps"
+            )
+
+        for epoch in range(starting_epoch, args.num_epochs):
+            model.train()
+            total_loss = 0
+
+            # if args.resume_from_checkpoint and epoch == starting_epoch and resume_step is not None:
+            #     # We need to skip steps until we reach the resumed step
+            #     train_dataloader = accelerator.skip_first_batches(train_dataloader, resume_step)
+            #     overall_step += resume_step
+
+            for step, batch in enumerate(train_dataloader):
+                outputs = model(**batch)
+                loss = outputs.loss / gradient_accumulation_steps
+                total_loss += loss.detach().float()
+                accelerator.backward(loss)
+                if step % gradient_accumulation_steps == 0:
+                    optimizer.step()
+                    lr_scheduler.step()
+                    optimizer.zero_grad()
+
+                if step % args.print_freq == 0:
+                    accelerator.print(
+                        f"[Training] Epoch: {epoch} | Step {step}/{len(train_dataloader)} - Loss: {outputs.loss:.2f} - LR: {optimizer.param_groups[0]['lr']:.7f}"
+                    )
+
+                if accelerator.is_main_process:
+                    if step % 100 == 0:
+                        model.eval()
+
+                        with torch.no_grad():
+                            with torch.autocast("cuda"):
+                                x = "\r\n- Bachelor’s Degree in Computer Science or related degree\r\n- 4+ years professional experience in software development\r\n- Computer Science fundamentals in object-oriented design\r\n- Computer Science fundamentals in data structures\r\n- Computer Science fundamentals in algorithm design, problem solving, and complexity analysis\r\n- Proficiency in Java, C#, Scala, or other similar programming languages\r\n"
+                                golden_sample = f"Write a modern and engaging job posting for the following basic qualifications: {x}\r\nResponse: \r\n"
+
+                                inputs = tokenizer(golden_sample, return_tensors="pt")
+                                outputs = accelerator.unwrap_model(model).generate(
+                                    input_ids=inputs["input_ids"].to("cuda"),
+                                    do_sample=True,
+                                    temperature=0.9,
+                                    max_length=1024,
+                                )
+                                print(
+                                    tokenizer.batch_decode(
+                                        outputs.detach().cpu().numpy(), skip_special_tokens=True
+                                    )
+                                )
+                        model.train()
+
+            adapter_weights = get_peft_model_state_dict(model=accelerator.unwrap_model(model))
+
+            if args.output_dir:
+                output_dir = os.path.join(args.output_dir, f"epoch_{epoch}")
+                accelerator.save_state(output_dir)
+                torch.save(adapter_weights, os.path.join(output_dir, "adapter.pth"))
+
+            if args.model_dir:
+                model_dir = os.path.join(args.model_dir, "model")
+                accelerator.save_state(model_dir)
+                torch.save(adapter_weights, os.path.join(model_dir, "adapter.pth"))
+
+            accelerator.print(
+                f"[Training] Epoch: {epoch} Completed | Loss: {(total_loss / (len(train_dataloader))):.2f} - LR: {optimizer.param_groups[0]['lr']:.7f}"
+            )
+
+    # batch_size is not needed here, as the decorator will set it
+    # pylint: disable=no-value-for-parameter
+    training_loop()
+
+    # accelerator.end_training()
+    accelerator.wait_for_everyone()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Multilabel text classification")
+
+    parser.add_argument("--model_name", default="EleutherAI/pythia-2.8b", type=str)
+
+    parser.add_argument(
+        "--batch_size",
+        default=2,
+        type=int,
+        help="the training will find the max batch size that fits into memory and use gradient accumulation",
+    )
+    parser.add_argument(
+        "--num_epochs", default=10, type=int, metavar="N", help="number of total epochs to run"
+    )
+    parser.add_argument("--seq_len", default=1024, type=int, help="sequence length")
+    parser.add_argument("--learning_rate", default=0.0005, type=float, help="initial learning rate")
+    parser.add_argument("--seed", default=42, type=int, help="seed for initializing training. ")
+
+    # LoRa
+    parser.add_argument("--lora_r", default=8, type=int, help="LoRa r")
+    parser.add_argument("--lora_alpha", default=16, type=int, help="LoRa alpha")
+    parser.add_argument("--lora_dropout", default=0.1, type=float, help="LoRa dropout")
+
+    parser.add_argument("--print_freq", default=10, type=int, help="print frequency")
+    parser.add_argument("--output_dir", default=os.environ.get("SM_OUTPUT_DIR"))
+    parser.add_argument("--model_dir", default=os.environ.get("SM_MODEL_DIR"))
+    parser.add_argument("--resume", default="", help="resume from checkpoint")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main(parse_args())
